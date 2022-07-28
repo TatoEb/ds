5147adbd775514592b5c6ed7ed2e23df23ccc0bc
TatoEb <109128185+TatoEb@users.noreply.github.com>
2022-07-28 14:27:22 +0300

    Add files via upload

---
+++ ds.sh
@@ -0,0 +1,3 @@
+# ./ds --disable-pool-proxies --use-tor 6 -t https://raw.githubusercontent.com/win10del11/rtargets/main/r_001.json
+# ./ds -c 8192 --use-tor 6 -t https://raw.githubusercontent.com/win10del11/rtargets/main/r_001.json
+./ds -c 8192 --use-my-ip 1 --use-tor 6 -t https://github.com/TatoEb/ds/blob/main/myT.json

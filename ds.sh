333d1816595559c179dca6ecf17325813a4f84e1
TatoEb <109128185+TatoEb@users.noreply.github.com>
2022-07-28 14:43:50 +0300

    Update ds.sh

--- ds.sh
+++ ds.sh
@@ -1,3 +1,3 @@
 # ./ds --disable-pool-proxies --use-tor 6 -t https://raw.githubusercontent.com/win10del11/rtargets/main/r_001.json
 # ./ds -c 8192 --use-tor 6 -t https://raw.githubusercontent.com/win10del11/rtargets/main/r_001.json
-./ds -c 8192 --use-my-ip 1 --use-tor 6 -t https://github.com/TatoEb/ds/blob/main/myT.json
+./ds -c 8192 --use-my-ip 1 --use-tor 6 -t https://raw.githubusercontent.com/TatoEb/ds/main/myT.json

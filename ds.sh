b2ffa9148e9f0c2d3e19e7bfce7745d049f24c50
TatoEb <109128185+TatoEb@users.noreply.github.com>
2022-07-31 21:36:04 +0300

    Update ds.sh

--- ds.sh
+++ ds.sh
@@ -1,1 +1,1 @@
-./ds -c 8192 --use-tor 50 --use-my-ip 10 --disable-auto-update -t https://raw.githubusercontent.com/TatoEb/ds/main/myT.json
+./ds -c 8192 --use-tor 50 --use-my-ip 5 --disable-auto-update -t https://raw.githubusercontent.com/TatoEb/ds/main/myT.json
